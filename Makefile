fe3f90625831a5ca21f45c62ef41693b2e13670f
George Shapovalov <gshapovalov@gmail.com>
2018-10-30 13:42:30 +0100

    initial import of files, added top levels so far. Redone the top level tests file

--- Makefile
+++ Makefile
@@ -4,3 +4,8 @@ SOURCES = src/*.ad?
 # rule to link the program
 abf_io: $(SOURCES)
 	gprbuild -P abf_io.gpr
+
+clean:
+	rm obj/dbg/* obj/release/* $(TARGET)
+
+.PHONEY: clean
--- src/run_abf_io.adb
+++ src/run_abf_io.adb
@@ -1,6 +1,6 @@
 --
--- <one line to give the program's name and a brief idea of what it does.>
--- 
+-- Runs some tests of ABT io routines.
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -11,17 +11,24 @@ with Ada.Command_Line, GNAT.Command_Line;
 with Ada.Directories, Ada.Environment_Variables;
 with Ada.Text_IO, Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
--- all methods in .Unbounded are easy to ident and have unique names, no need to hide visibility
+-- all methods in .Unbounded have unique names, no need to hide visibility
+
+with ABF.Header.Waveform; -- use ABF.Header; use ABF.Header.Waveform; use ABF;
+with ABF.Data; -- use ABF.Data;
+with C; use C;
+with util; use util;
 
 
 procedure run_abf_io is
     procedure printUsage is
         use Ada.Text_IO;
     begin
-        Put_Line ("!!!DESCRIPTION!!!");
+        Put_Line ("Run some general ABF IO tests.");
+        Put_Line ("Replace test_data.abf with any of the provided files in dat/. ");
+        Put_Line ("If out.atf filename is provided, then contents is also written into the atf file.");
         New_Line;
         Put_Line ("usage:");
-        Put_Line ("   " & Ada.Command_Line.Command_Name & " [-h -g -n: -v]  positional");
+        Put_Line ("   " & Ada.Command_Line.Command_Name & " [-h -g -n: -v]  tests_data.abf [out.atf]");
         New_Line;
         Put_Line ("options:");
         --  only short options for now
@@ -31,28 +38,21 @@ procedure run_abf_io is
         Put_Line ("-n      enter some number");
     end printUsage;
 
-    Finish : exception;  -- normal termination
-    --  Ada doesn't have an analog of sys.exit() in the standard, because of multitasking
-    --  support directly by the language. So just raise this anywhere and catch at the top..
-    --
-    --  If you really miss this function, OS_Exit/OS_Abort are defined in
-    --  GNAT.OS_Lib (one of gnat extensions).  Just be aware that it does not play well with Spawn and other
-    --  process-controll features!!
+    Finish : exception;
+    -- raised in processCommandLine in case of inconsistent parameters passed
+    -- serves to skip all further action without producing ugly tracebacks..
+    -- (Ada soe not have an analog of sys.exit()  by design. The execution flow is supposed to be explicit)
 
     type ParamRec is record
-		--  mostly the commandline params. DepGraph and USE flags will go as separate vars
-        name    : Unbounded_String := Null_Unbounded_String;
-        workDir : Unbounded_String := Null_Unbounded_String;
+        inName, outName : Unbounded_String := Null_Unbounded_String;
         val     : Integer := 0;
         Debug   : Boolean := False;
     end record;
 
     procedure processCommandLine (params : in out ParamRec) is
-        --  this works very similarly to GNU getopt, except this one uses single '-'
-        --  for both short and long options
-        use Ada.Command_Line, GNAT.Command_Line, Ada.Text_IO, Ada.Integer_Text_IO;
-        Options : constant String := "g h n: v";
-        Last:Positive;
+        use Ada.Command_Line, GNAT.Command_Line, Ada.Text_IO;
+        Options : constant String := "g h v";
+        nameCount : Natural := 0;
     begin
         if Argument_Count < 1 then
             printUsage;
@@ -62,30 +62,24 @@ procedure run_abf_io is
             loop
                 case Getopt (Options) is
                     when ASCII.NUL =>
-                        exit;
-                    --  end of option list
+                        exit; --  end of option list
 
                     when 'g' | 'v' => params.Debug := True;
                     when 'h' =>
                         printUsage;
                         raise Finish;
 
-                    when 'n' => Get(Parameter,Positive(params.val),Last);
-
                     when others =>
                         raise Program_Error;
-                        --  serves to catch "damn, forgot to include that option here"
+                        --  to catch an option in Options string but not in case statement
                 end case;
             end loop;
         exception
             when Invalid_Switch =>
-                Put_Line ("Invalid Switch " & Full_Switch);
+                Put_Line ("Unrecognized option passed: " & Full_Switch);
                 raise Finish;
             when Invalid_Parameter =>
                 Put_Line ("No parameter for " & Full_Switch);
-                raise Finish;
-            when Data_Error =>
-                Put_Line ("Invalid numeric format for switch" & Full_Switch);
                 raise Finish;
         end;
         -- get positional params
@@ -94,39 +88,60 @@ procedure run_abf_io is
                 S : constant String := Get_Argument (Do_Expansion => True);
             begin
                 exit when S'Length = 0;
-                if params.Debug then Put_Line ("alternative file was passed: '" & S &"'"); end if;
-                params.name := To_Unbounded_String(S);
+                nameCount := nameCount + 1;
+                if nameCount = 1 then
+                    DebugPrint(params.Debug, "processing file: '" & S &"'");
+                    params.inName := To_Unbounded_String(S);
+                elsif nameCount = 2 then
+                    DebugPrint(params.Debug, "output file: '" & S &"'");
+                    params.outName := To_Unbounded_String(S);
+                else
+                    DebugPrint(params.Debug, "extra names passed, ignoring..");
+                end if;
             end;
         end loop;
-        --  process env vars
-        declare -- just a visibility wrapper
-            use Ada.Directories;
-        begin
-            if Ada.Environment_Variables.Exists ("ENV_VAR") then
-                params.WorkDir :=
-                    To_Unbounded_String (Ada.Environment_Variables.Value ("ENV_VAR"));
-            else
-                params.WorkDir := To_Unbounded_String (Current_Directory);
-            end if;
-            --
-            if not Exists (To_String (params.WorkDir))
-               or else Kind (To_String (params.WorkDir)) /= Directory
-            then
-                Put_Line
-                   ("The directory " & To_String (params.WorkDir) & " does not exist!");
-                raise Finish;
-            end if;
-        end;
     end processCommandLine;
 
 
     params : ParamRec;
 
+    abfH : ABF.Header.ABFFileHeader;
+    wf   : ABF.Header.Waveform.WaveformRec;
+    modeDesc : ABF.Mode_Descriptors;
+    data : ABF.Data.File_Contents;
+
     use Ada.Text_IO;
 
-begin  -- main
+begin
     processCommandLine (params);
-    Put_Line("basic test");
+    Put_Line("commencing tests");
+    Put("reading header .. ");
+    abfH := ABF.Header.Read_Header(To_String(params.inName));
+    Put_Line("done.");
+    --
+    Put("reading waveform .. ");
+    wf := ABF.Header.Waveform.GetWafeform(abfH);
+    Put_Line("done.");
+    --
+    Put("reading mode descriptors .. ");
+    modeDesc := ABF.Header.GetModeDescriptors(abfH);
+    Put_Line("done.");
+    --
+    Put("reading data .. ");
+    data := ABF.Data.ReadABFFile(To_String(params.inName));
+    Put_Line("done.");
+    --
+    --	Put("ABFFileHeader'Size=");Put(ABFFileHeader'Size/8);New_Line;
+    ABF.Header.PrintPrincialParams(abfH);
+    --	PrintWaveform(wf);
+    --	PrintModeDescriptors(md, File=>Standard_Output);
+    -- 	PrintDigiTrainParams(dt, File=>Standard_Output);
+    -- 	PrintData(data);
+    if params.outName /= Null_Unbounded_String then
+        ABF.Data.WriteAtf(To_String(params.outName), data);
+--     else
+--         WriteAtf("test_data.atf", data);
+    end if;
 exception
 	when Finish => null;
 end run_abf_io;
